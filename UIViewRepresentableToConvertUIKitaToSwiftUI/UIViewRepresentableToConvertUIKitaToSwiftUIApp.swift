e0954beef7a8d315d026e61904c8b475d0f98330
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-07 19:40:58 +0300

    Initial Commit

---
+++ UIViewRepresentableToConvertUIKitaToSwiftUI/UIViewRepresentableToConvertUIKitaToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewRepresentableToConvertUIKitaToSwiftUIApp.swift
+//  UIViewRepresentableToConvertUIKitaToSwiftUI
+//
+//  Created by ramil on 07.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct UIViewRepresentableToConvertUIKitaToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
